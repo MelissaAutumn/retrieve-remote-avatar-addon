3935284e9ec703b3e76b4467db2c2ed66a524824
Melissa Autumn <all@melissaautumn.com>
2025-06-28 11:41:32 -0700

    Refactor to retrieve urls in background.js, and cache results.

--- src/background.js
+++ src/background.js
@@ -1,14 +1,105 @@
+const GRAVATAR_URL = 'https://gravatar.com/avatar/$hash?d=404';
+const LIBRAVATAR_URL = 'https://seccdn.libravatar.org/avatar/$hash?d=404';
+const CACHED_NOT_FOUND = '[not found]';
+
+/**
+ * Compute sha256 hash from a given input string
+ * Source: https://stackoverflow.com/a/67600346
+ * @param input
+ * @returns {Promise<string>}
+ */
+const sha256 = async (input) => {
+  const textAsBuffer = new window.TextEncoder().encode(input);
+  const hashBuffer = await window.crypto.subtle.digest('SHA-256', textAsBuffer);
+  const hashArray = Array.from(new Uint8Array(hashBuffer));
+  const hash = hashArray
+    .map((item) => item.toString(16).padStart(2, '0'))
+    .join('');
+  return hash;
+};
+
+const hashEmail = async (email) => {
+  // Sometimes email contains "my name <my@email.com>", so process that...
+  const emailIdx = email.indexOf('<');
+  const processedEmail = emailIdx > -1 ? email.substring(emailIdx + 1, email.length - 1) : email;
+  const address = String(processedEmail).trim().toLowerCase();
+  return await sha256(address);
+};
+
+const getRemoteURL = async (email, rootUrl) => {
+  const hashedEmail = await hashEmail(email);
+  const url = rootUrl.replace('$hash', hashedEmail);
+  const response = await window.fetch(url);
+  if (!response.ok) {
+    return null;
+  }
+
+  const blob = await response.blob();
+  // .bytes is available in firefox, so should be okay here.
+  const bytes = await blob.bytes();
+  const base64String = btoa(String.fromCharCode(...bytes));
+  return `data:image/png;base64,${base64String}`;
+};
+
+const getCache = async (email) => {
+  const key = `imgv1_${await hashEmail(email)}`;
+  const item = await browser.storage.local.get(key);
+  if (!item || !item.hasOwnProperty(key)) {
+    return null;
+  }
+  const { expiresAt, data } = item[key];
+
+  if (expiresAt <= Date.now()) {
+    await browser.storage.local.remove(key);
+    return null;
+  }
+  return data;
+};
+
+const setCache = async (email, dataURL, expiresAtDays = 3) => {
+  const key = `imgv1_${await hashEmail(email)}`;
+  const expiresAt = new Date(Date.now());
+  expiresAt.setDate(expiresAt.getDate() + expiresAtDays);
+  const item = {
+    expiresAt: expiresAt.getTime(),
+    data: dataURL
+  };
+  await browser.storage.local.set({ [key]: item });
+};
+
+const getAvatarURL = async (email) => {
+  try {
+    const cachedUrl = await getCache(email);
+    if (cachedUrl) {
+      return cachedUrl === CACHED_NOT_FOUND ? null : cachedUrl;
+    }
+  } catch (e) {
+    console.warn('Error retrieving cached avatar', e);
+  }
+
+  const url = await getRemoteURL(email, LIBRAVATAR_URL) ?? await getRemoteURL(email, GRAVATAR_URL);
+  try {
+    await setCache(email, url === null ? CACHED_NOT_FOUND : url, url === null ? 1 : 3);
+  } catch (e) {
+    console.warn('Error setting cached avatar', e);
+  }
+  return url;
+};
+
 /**
  * command handler: handles the commands received from the content script
  */
 const doHandleCommand = async (message, sender) => {
-  //const { command } = message;
   const {
     tab: { id: tabId },
   } = sender;
 
   const messageHeader = await browser.messageDisplay.getDisplayedMessage(tabId);
-  browser.com_melissaautumn_msgHdr.setRemoteAvatar(messageHeader.author);
+  const url = await getAvatarURL(messageHeader.author);
+  if (!url) {
+    return;
+  }
+  await browser.com_melissaautumn_msgHdr.setRemoteAvatar(url);
 };
 
 /**
@@ -22,7 +113,7 @@ const doHandleCommand = async (message, sender) => {
  *    the appropriate handler
  */
 browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
-  if (message && message.hasOwnProperty("command")) {
+  if (message && message.hasOwnProperty('command')) {
     // If we have a command, return a promise from the command handler.
     return doHandleCommand(message, sender);
   }
@@ -34,6 +125,6 @@ browser.runtime.onMessage.addListener((message, sender, sendResponse) => {
  * whenever a message is displayed
  */
 messenger.messageDisplayScripts.register({
-  js: [{ file: "/src/on_message_display.js" }],
+  js: [{ file: '/src/on_message_display.js' }],
 });
 
--- src/com_melissaautumn_msgHdr/api.js
+++ src/com_melissaautumn_msgHdr/api.js
@@ -1,19 +1,3 @@
-/**
- * Compute sha256 hash from a given input string
- * Source: https://stackoverflow.com/a/67600346
- * @param window
- * @param input
- * @returns {Promise<string>}
- */
-const sha256 = async (window, input) => {
-  const textAsBuffer = new window.TextEncoder().encode(input);
-  const hashBuffer = await window.crypto.subtle.digest("SHA-256", textAsBuffer);
-  const hashArray = Array.from(new Uint8Array(hashBuffer));
-  const hash = hashArray
-    .map((item) => item.toString(16).padStart(2, "0"))
-    .join("");
-  return hash;
-};
 
 /**
  * Navigate the confusing forbidden labrynth to retrieve the avatar's element.
@@ -33,83 +17,16 @@ const getAvatarElement = (rootDoc) => {
   return messageBrowserDoc.querySelector('.recipient-avatar')
 }
 
-/**
- * Construct the gravatar url
- * window is required as we don't have a global window object here.
- * @param window
- * @param email
- * @returns {Promise<string>}
- */
-const getGravatarURL = async ( window, email )  => {
-  // Trim leading and trailing whitespace from
-  // an email address and force all characters
-  // to lower case
-  const address = String( email ).trim().toLowerCase();
-
-  // Create a SHA256 hash of the final string
-  const hash = await sha256( window, address );
-
-  const url =  `https://gravatar.com/avatar/${ hash }?d=404`;
-
-  const response = await window.fetch(url);
-  if (!response.ok) {
-    return null;
-  }
-  const blob = await response.blob();
-  return window.URL.createObjectURL(blob);
-}
-
-const getLibravatarURL = async ( window, email )  => {
-  // Trim leading and trailing whitespace from
-  // an email address and force all characters
-  // to lower case
-  const address = String( email ).trim().toLowerCase();
-
-  // Create a SHA256 hash of the final string
-  const hash = await sha256( window, address );
-
-  const url =  `https://seccdn.libravatar.org/avatar/${ hash }?d=404`;
-
-  const response = await window.fetch(url);
-  if (!response.ok) {
-    return null;
-  }
-  const blob = await response.blob();
-  return window.URL.createObjectURL(blob);
-}
 
 var com_melissaautumn_msgHdr = class extends ExtensionCommon.ExtensionAPI {
   getAPI (context) {
     return {
       com_melissaautumn_msgHdr: {
-        async setRemoteAvatar (email) {
-          console.log(context.extension);
+        async setRemoteAvatar (url) {
           const doc = Services.wm.getMostRecentWindow('mail:3pane').document
-          //const win = Services.wm.getMostRecentWindow('mail:3pane').window;
-
-          // ...the mail body allows remote images (if you allow it)
-          // FIXME: Do remote requests in background.js or somewhere that allows remote fetches
-          const win = doc.getElementById('mail3PaneTabBrowser1').browsingContext.window
-
           const avatar = getAvatarElement(doc);
           // if returned null we failed, or they already have an avatar set.
-          if (!avatar) {
-            return;
-          }
-
-          // Sometimes email contains "my name <my@email.com>", so process that...
-          const emailIdx = email.indexOf('<');
-          const processedEmail = emailIdx > -1 ? email.substring(emailIdx+1, email.length-1) : email;
-
-          // TODO: Cache the result for a period of time
-
-          // Retrieve libravatar, and if that fails try gravatar
-          let url = await getLibravatarURL(win, processedEmail);
-          if (!url) {
-            url = await getGravatarURL(win, processedEmail);
-          }
-          // If neither works then return
-          if (!url) {
+          if (!avatar || !url) {
             return;
           }
 
